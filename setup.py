eb1be2dc334677b6c81b6822f53b85659ea028fc
Kristjan Kongas <kongaskristjan@gmail.com>
2023-10-18 18:15:49 +0300

    Add requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
+from typing import List
 from setuptools import setup, find_packages
+
+def get_requirements() -> List[str]:
+    return [
+        'pytest>=7.0.0',
+    ]
 
 setup(
     name='pytest-unitvis',
-    version='0.1',
+    version='0.0.1',
     packages=find_packages(),
     entry_points={'pytest11': ['unitvis = unitvis.plugin']},
+    install_requires=get_requirements(),
 )
