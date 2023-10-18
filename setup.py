db272c3ee036611af624c2195691daf848664def
Kristjan Kongas <kongaskristjan@gmail.com>
2023-10-18 18:43:46 +0300

    Add more info to the pypi package

--- setup.py
+++ setup.py
@@ -8,10 +8,29 @@ def get_requirements() -> List[str]:
 
 setup(
     name='pytest-unitvis',
-    version='0.0.1',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
+    version='0.0.2',
     packages=find_packages(),
     entry_points={'pytest11': ['unitvis = unitvis.plugin']},
+
+    python_requires='>=3.8',
     install_requires=get_requirements(),
+
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+
+    license='MIT',
+    license_files=['LICENSE'],
+    author='Kristjan Kongas',
+
+    url='https://github.com/kongaskristjan/pytest-unitvis',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
 )
