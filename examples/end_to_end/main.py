Fix y-coordinate of hands; fix hour hand's not taking minutes into account; fix pixel augmentations interfering with mean/var normalization

examples/end_to_end/main.py
@@ -1,7 +1,7 @@
 import math
 import random
 from pathlib import Path
-from typing import List, Tuple, Dict
+from typing import Dict, List, Tuple
 
 import torch
 import torch.nn as nn
@@ -44,20 +44,22 @@ class ClockDataset(Dataset[Tuple[Tensor, "Time"]]):
     Dataset for clock images. The labels are Time objects representing the time shown on the clock.
     """
 
-    def __init__(self, data_dir: Path):
+    def __init__(self, data_dir: Path, normalize: bool = True):
         super().__init__()
         self.paths = []
         for pth in data_dir.glob("*"):
             if pth.is_file():
                 self.paths.append(pth)
         self.paths = sorted(self.paths)
+        self.normalize = normalize
 
     def __getitem__(self, index: int) -> Tuple[Tensor, "Time"]:
         path = self.paths[index]
 
         image = Image.open(path)
         image_tensor = F.to_image(image).to(torch.float32) / 255  # type: ignore
-        image_tensor = normalize_image(image_tensor)
+        if self.normalize:
+            image_tensor = normalize_image(image_tensor)
         label = get_label(path)
 
         return image_tensor, label
@@ -73,7 +75,7 @@ class ClockCoordinateDataset(Dataset[Tuple[Tensor, Tensor]]):
 
     def __init__(self, data_dir: Path, augment: bool = False):
         super().__init__()
-        self.clock_dataset = ClockDataset(data_dir)
+        self.clock_dataset = ClockDataset(data_dir, normalize=False)
         self.augment = augment
 
     def __getitem__(self, index: int) -> Tuple[Tensor, Dict[str, Tensor]]:
@@ -81,8 +83,9 @@ class ClockCoordinateDataset(Dataset[Tuple[Tensor, Tensor]]):
         coords = label.get_coords()
         if self.augment:
             image, coords = augment(image, coords)
+        image = normalize_image(image)
         return image, coords
-    
+
     def __len__(self) -> int:
         return len(self.clock_dataset)
 
@@ -109,11 +112,11 @@ class Time:
 
     def get_coords(self) -> Dict[str, Tensor]:
         # Return a tensor of shape (4,) with the coordinates of the hour and minute hands.
-        hour_angle = 2 * math.pi * (self.hour % 12) / 12
-        hour_xy = (math.sin(hour_angle), math.cos(hour_angle))
+        hour_angle = 2 * math.pi * (self.hour + self.minute / 60) / 12
+        hour_xy = (math.sin(hour_angle), -math.cos(hour_angle))
         minute_angle = 2 * math.pi * self.minute / 60
-        minute_xy = (math.sin(minute_angle), math.cos(minute_angle))
-        return {"hours": Tensor(hour_xy), "minutes": Tensor(minute_xy)}
+        minute_xy = (math.sin(minute_angle), -math.cos(minute_angle))
+        return {"hour": Tensor(hour_xy), "minute": Tensor(minute_xy)}
 
     def approx_eq(self, other: "Time", max_diff_minutes: int = 5) -> bool:
         first = self.hour * 60 + self.minute
@@ -146,7 +149,7 @@ def get_label(path: Path) -> Time:
 
 
 def augment(image: Tensor, label: Dict[str, Tensor]) -> Tuple[Tensor, Dict[str, Tensor]]:
-    hour_xy, minute_xy = label["hours"], label["minutes"]
+    hour_xy, minute_xy = label["hour"], label["minute"]
 
     # Randomly flip horizontally
     if random.random() > 0.5:
@@ -176,7 +179,7 @@ def augment(image: Tensor, label: Dict[str, Tensor]) -> Tuple[Tensor, Dict[str, 
     saturation_factor = random.uniform(0.8, 1.2)
     image = F.adjust_saturation(image, saturation_factor)
 
-    return image, {"hours": hour_xy, "minutes": minute_xy}
+    return image, {"hour": hour_xy, "minute": minute_xy}
 
 
 # Model

examples/end_to_end/test_main.py
@@ -1,12 +1,20 @@
 from pathlib import Path
 from typing import List
 
-from torch import Tensor
-from PIL import Image
+import cv2
 import numpy as np
+from PIL import Image
+from torch import Tensor
 
-from examples.end_to_end.main import ClockDataset, ClockCoordinateDataset, Time, get_label, mean_norm, std_norm
-from visual.interface import VisualFixture, fix_seeds, visual, standardize
+from examples.end_to_end.main import (
+    ClockCoordinateDataset,
+    ClockDataset,
+    Time,
+    get_label,
+    mean_norm,
+    std_norm,
+)
+from visual.interface import VisualFixture, fix_seeds, standardize, visual
 
 test_data_path = Path("examples/end_to_end/test_data")
 
@@ -17,20 +25,40 @@ def test_ClockDataset(visual: VisualFixture, fix_seeds):
     for image, label in dataset:
         images.append(standardize(image.numpy(), mean_denorm=mean_norm, std_denorm=std_norm))
         labels.append(str(label))
+
+    visual.print("Non-augmented dataset with time labels:")
     visual.show_images(images, labels)
 
 
-#def test_ClockCoordinateDataset(visual: VisualFixture, fix_seeds):
-#    dataset = ClockCoordinateDataset(test_data_path / "train")
-#
-#def visualize_dataset(visual: VisualFixture, dataset: ClockCoordinateDataset):
-#    images: List[np.ndarray] = []
-#    for image, label in dataset:
-#        image = standardize(image.numpy(), mean_denorm=mean_norm, std_denorm=std_norm)
-#        images.append(image)
-#
-#    visual.show_images(images)
+def test_ClockCoordinateDataset(visual: VisualFixture, fix_seeds):
+    dataset_non_aug = ClockCoordinateDataset(test_data_path / "train")
+    visual.print("Non-augmented dataset with hands drawn:")
+    visualize_dataset(visual, dataset_non_aug)
 
+    dataset_aug = ClockCoordinateDataset(test_data_path / "train", augment=True)
+    visual.print("Augmented dataset with hands drawn:")
+    visualize_dataset(visual, dataset_aug)
+
+
+def visualize_dataset(visual: VisualFixture, dataset: ClockCoordinateDataset):
+    images: List[np.ndarray] = []
+    for image, label in dataset:
+        image = standardize(image.numpy(), mean_denorm=mean_norm, std_denorm=std_norm)
+
+        # Draw clock hands as lines
+        image = image.copy()
+        for hand_name in label:
+            color = {"minute": (0, 255, 0), "hour": (255, 0, 0)}[hand_name]
+            length = {"minute": 0.4, "hour": 0.2}[hand_name]
+
+            coords = label[hand_name].numpy()
+            center = np.array([image.shape[0] / 2, image.shape[1] / 2])
+            end = center + length * coords * image.shape[0]
+            cv2.line(image, tuple(center.astype(int)), tuple(end.astype(int)), color, 1)
+
+        images.append(image)
+
+    visual.show_images(images)
 
 
 def test_get_label():

visual/interface.py
@@ -1,5 +1,5 @@
 import random
-from typing import Generator, List, Optional, Union
+from typing import Generator, List, Optional
 
 import numpy as np
 import pytest
@@ -7,8 +7,8 @@ from _pytest.fixtures import FixtureRequest
 from plotly.graph_objs import Figure
 
 from visual.lib.convenience import (
-    create_plot_from_images,
     correct_layout,
+    create_plot_from_images,
     get_grid_shape,
     get_image_max_value_from_type,
     get_layout_from_image,
@@ -73,9 +73,7 @@ class VisualFixture:
         assert len(images) > 0, "At least one image must be specified"
 
         grid_shape = get_grid_shape(len(images), max_cols)
-        fig = create_plot_from_images(
-            images, labels, grid_shape, height_per_row * grid_shape[0]
-        )
+        fig = create_plot_from_images(images, labels, grid_shape, height_per_row * grid_shape[0])
         self.show(fig)
 
 
@@ -124,7 +122,9 @@ def visual(request: FixtureRequest, visual_UI: UI) -> Generator[VisualFixture, N
     else:
         pytest.skip("Visualization is not enabled, add --visual option to enable")
 
+
 # Convenience features
+
 
 @pytest.fixture
 def fix_seeds() -> None:
@@ -156,6 +156,7 @@ def fix_seeds() -> None:
     except ImportError:
         pass
 
+
 def standardize(
     image: np.ndarray,
     layout: Optional[str] = None,
@@ -165,16 +166,16 @@ def standardize(
     max_value: Optional[float] = None,
 ) -> np.ndarray:
     """
-        - layout (Optional[str]): The shape of the images. If not specified, the shape is
-            determined automatically. Supported shapes are "hwc", "chw", "hw", "1chw", "1hwc".
-        - mean_comp (Optional[List[float]]): The mean that was used to normalize the images, which
-            is used to denormalize the images. If not specified, the images are not denormalized.
-        - std_comp (Optional[List[float]]): The standard deviation that was used to normalize the
-            images, which is used to denormalize the images. If not specified, the images are not
-            denormalized.
-        - min_value (float): The assumed minimum value of the images.
-        - max_value (Optional[float]): The assumed maximum value of the images. If not specified,
-            the maximum value is 1 for float images and 255 for integer images.
+    - layout (Optional[str]): The shape of the images. If not specified, the shape is
+        determined automatically. Supported shapes are "hwc", "chw", "hw", "1chw", "1hwc".
+    - mean_comp (Optional[List[float]]): The mean that was used to normalize the images, which
+        is used to denormalize the images. If not specified, the images are not denormalized.
+    - std_comp (Optional[List[float]]): The standard deviation that was used to normalize the
+        images, which is used to denormalize the images. If not specified, the images are not
+        denormalized.
+    - min_value (float): The assumed minimum value of the images.
+    - max_value (Optional[float]): The assumed maximum value of the images. If not specified,
+        the maximum value is 1 for float images and 255 for integer images.
     """
 
     # Get layout and max value