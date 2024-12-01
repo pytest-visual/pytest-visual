429e2f8ae8c795b289dcb34e3daccf2ab2838b55
Kristjan Kongas <kongaskristjan@gmail.com>
2024-12-01 14:58:51 +0200

    Update Python version from 3.8-3.12 to 3.9-3.13

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
